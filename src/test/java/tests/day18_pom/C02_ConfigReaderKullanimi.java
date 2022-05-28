dc77c6d0ac299a6ae92bfbee6d56ab60251a23e8
Oner Tekin <tekinoner37@gmail.com>
2022-05-28 22:44:37 +0300

    28.05.2022

---
+++ src/test/java/tests/day18_pom/C02_ConfigReaderKullanimi.java
@@ -0,0 +1,39 @@
+package tests.day18_pom;
+
+import com.github.javafaker.Faker;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.FacebookPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class C02_ConfigReaderKullanimi {
+    @Test
+    public void test() throws InterruptedException {
+
+        FacebookPage facebookPage=new FacebookPage();
+        //facebook anasayfaya git
+        Driver.getDriver().get(ConfigReader.getProperty("facebookUrl"));
+
+        // kullanici mail kutusuna rastgele bir isim yazdirin
+       facebookPage.kullaniciMail.sendKeys(ConfigReader.getProperty("fbWrongUsername"));
+
+
+        // kullanici sifre kutusuna rastgele bir password yazdirin
+facebookPage.password.sendKeys(ConfigReader.getProperty("fbWrongPassword"));
+
+        // login butonuna basin
+facebookPage.login.click();
+
+
+        // giris yapilamadigini test edin
+        Assert.assertTrue(facebookPage.girilemediYaziElementi.isDisplayed());
+
+        //sayfayi kapatin
+        Driver.closeDriver();
+
+
+
+    }
+}
+
