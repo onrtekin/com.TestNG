8922a80d6b760af69645bdc714e7431238f4ee81
Oner Tekin <tekinoner37@gmail.com>
2022-06-01 20:35:13 +0300

    01.06.2022

--- src/test/java/tests/day19_smokeTest/NegatifLoginTest.java
+++ src/test/java/tests/day19_smokeTest/NegatifLoginTest.java
@@ -31,4 +31,50 @@ public class NegatifLoginTest {
 
 
     }
+    @Test
+    public void yanlisEmail() {
+        // Bir test method olustur positiveLoginTest()
+        //       https://www.bluerentalcars.com/ adresine git
+        Driver.getDriver().get(ConfigReader.getProperty("brcUrl"));
+
+        //      login butonuna bas
+        brcPage.ilkLoginButonu.click();
+
+        //test data user email: customer@bluerentalcars.com ,
+        brcPage.emailTextBox.sendKeys(ConfigReader.getProperty("brcWrongEmail"));
+        //test data password : 12345
+        brcPage.passwordTextBox.sendKeys(ConfigReader.getProperty("brcValidPassword"));
+
+        //login butonuna tiklayin
+        brcPage.ikinciLoginButonu.click();
+
+        //Degerleri girildiginde sayfaya basarili sekilde girilemedigini test et
+        Assert.assertTrue(brcPage.ikinciLoginButonu.isDisplayed());
+
+
+    }
+    @Test
+    public void yanlisSifreEmail() {
+        // Bir test method olustur positiveLoginTest()
+        //       https://www.bluerentalcars.com/ adresine git
+        Driver.getDriver().get(ConfigReader.getProperty("brcUrl"));
+
+        //      login butonuna bas
+        brcPage.ilkLoginButonu.click();
+
+        //test data user email: customer@bluerentalcars.com ,
+        brcPage.emailTextBox.sendKeys(ConfigReader.getProperty("brcWrongEmail"));
+        //test data password : 12345
+        brcPage.passwordTextBox.sendKeys(ConfigReader.getProperty("brcWrongPassword"));
+
+        //login butonuna tiklayin
+        brcPage.ikinciLoginButonu.click();
+
+        //Degerleri girildiginde sayfaya basarili sekilde girilemedigini test et
+        Assert.assertTrue(brcPage.ikinciLoginButonu.isDisplayed());
+Driver.closeDriver();
+
+
+    }
+
 }
